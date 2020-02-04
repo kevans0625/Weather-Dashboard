5c18b547c5cf3fa3bb582a1dd716c858736d221f
Kristin Evans <kevans0625@gmail.com>
2020-02-04 09:29:35 -0600

    Creted an array of buttons to display the cities.

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,21 @@
 
 $(document).ready(function() {  
 
-  
+    var cities = ["Houston", "St. Louis", "Atlanta", "Miami", "San Diego", "Philly"]
+    // create a function to run display search cities 
+    function rendercities(){
+           
+        $(".city-view").empty();
+for( var i=0; i<cities.length; i++){
+    //create element
+    var cityEL = $("<button>");
+    //add attributes 
+    cityEL.attr("data-name", cities[i]);
+    cityEL.addClass("newCity");
+    //add text
+    cityEL.text(cities[i]);
+    //append elemenent 
+    $(".city-view").prepend(cityEL);
+}
+rendercities()
 });
